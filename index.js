26c973aed4576adf84d93644fc8379b0cc430e69
NicatMammadov <127016816+NicatMammadov@users.noreply.github.com>
2023-05-10 22:36:30 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script>
+let a=parseInt(prompt("ilk ededi yazin: "));
+let b=parseInt(prompt("iknci ededi yazin: "));
+let cem=0;
+let hasil=1;
+while(a<=b){
+cem+=a;
+hasil*=a;
+a++;
+}
+alert("cem: "+cem+" hasil: "+hasil);
+</script>
+</head>
+</html>
